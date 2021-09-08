8c8d1788a04f5a2708c0f8a9f668a56c9669938d
Shabaz Salmani <ssalmani@fortinet.com>
2021-09-08 18:31:49 +0530

    Update marketPlaceController.js

--- assets/js/marketPlaceController.js
+++ assets/js/marketPlaceController.js
@@ -5,8 +5,6 @@
   angular.module('fortinetMarketplace')
     .controller('marketplaceController', marketplaceController);
 
-    marketplaceController.$inject = ['$scope', '$http'];
-
     function marketplaceController ($scope, $http) {
       console.log('test loaded');
     }
