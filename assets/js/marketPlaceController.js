1dd98e1a08a10f536e8ae65d43962df5df5ff4bc
Shabaz Salmani <ssalmani@fortinet.com>
2021-09-08 20:00:25 +0530

    Update marketPlaceController.js

--- assets/js/marketPlaceController.js
+++ assets/js/marketPlaceController.js
@@ -8,6 +8,7 @@
     marketplaceController.$inject = ['$scope', '$http'];
     function marketplaceController ($scope, $http) {
       console.log('test loaded');
+      $scope.listItems = [];
       $http({
         method: 'GET',
         url: 'https://update.cybersponse.com/connectors/info/connectors.json',
@@ -15,7 +16,29 @@
           'Content-Type': 'application/json;charset=utf-8'
         }
       }).then(function (response) {
-        console.log(response);
+        if(response.status === 200){
+          angular.forEach(response.data, function(connector){
+            connector.type = 'connector';
+            $scope.listItems.push(connector);
+          });
+          $http({
+            method: 'GET',
+            url: 'https://update.cybersponse.com/fsr-widgets/widgets.json',
+            headers: {
+              'Content-Type': 'application/json;charset=utf-8'
+            }
+          }).then(function (response) {
+            console.log(response);
+            if(response.status === 200){
+              angular.forEach(response.data, function(widget){
+                widget.type = 'widget';
+                $scope.listItems.push(widget);
+              }); 
+            }
+          }, function (error) {
+            console.log(error);
+          });
+        }
       }, function (error) {
         console.log(error);
       });
