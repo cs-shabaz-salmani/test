838f22d04a64f51f799011ba3ee899a2393feaed
Shabaz Salmani <ssalmani@fortinet.com>
2021-09-08 19:51:16 +0530

    Update marketPlaceController.js

--- assets/js/marketPlaceController.js
+++ assets/js/marketPlaceController.js
@@ -4,9 +4,21 @@
 
   angular.module('fortinetMarketplace')
     .controller('marketplaceController', marketplaceController);
-
-    function marketplaceController () {
+  
+    marketplaceController.$inject = ['$scope', '$http'];
+    function marketplaceController ($scope, $http) {
       console.log('test loaded');
+      $http({
+        method: 'GET',
+        url: 'update.cybersponse.com/connectors/info/connectors.json',
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8'
+        }
+      }).then(function (response) {
+        console.log(response);
+      }, function (error) {
+        console.log(error);
+      });
     }
     
 })();
