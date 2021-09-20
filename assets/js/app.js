46db9994ae4d34a607217b4b1fdc7e981d3be6a0
Shabaz Salmani <ssalmani@fortinet.com>
2021-09-21 01:04:49 +0530

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -87,8 +87,8 @@
       allListItems += '</div><h5>' + listItem.display + '</h5><div class="card-footer"><a class="card-link" href="#" title="GitHub Page" target="_blank"> <span class="fa fa-github"></span></a>';
       allListItems += '<a class="card-link" href="#" title="Forks" target="_blank"> <span class="fa fa-code-fork"></span>' + listItem.forks_count + '</a>';
       allListItems += '<a class="card-link" href="#" title="Stargazers" target="_blank"> <span class="fa fa-star"></span>' + listItem.stargazers_count + '</a></div></div>';
-      var marketPlace = document.getElementById('marketplace-list');
-      console.log(marketPlace);
-      marketPlace.append(allListItems);
     });
+    var marketPlace = document.getElementById('marketplace-list');
+    console.log(marketPlace);
+    marketPlace.append($(allListItems).html());
   }
