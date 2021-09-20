b3da7d4d2b0186356c37a26cc54584775cf9c8f3
Shabaz Salmani <ssalmani@fortinet.com>
2021-09-21 00:40:54 +0530

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,23 +1,90 @@
 'use strict';
 
-(function() {
-  var app = angular.module('fortinetMarketplace', []);
-    app.config(['$rootScopeProvider', '$locationProvider', '$httpProvider', '$qProvider', '$logProvider', '$controllerProvider',
-      function($rootScopeProvider, $locationProvider, $httpProvider, $qProvider, $logProvider, $controllerProvider) {
-        $locationProvider.html5Mode(true);
-        $logProvider.debugEnabled(false);
-        $rootScopeProvider.digestTtl(20);
-        $qProvider.errorOnUnhandledRejections(false);
-        if (window.localStorage.DEBUG) {
-          $logProvider.debugEnabled(true);
-          $qProvider.errorOnUnhandledRejections(true);
-        }
-  
-        app._controller = app.controller;
-        app.controller = function(name, constructor) {
-          $controllerProvider.register(name, constructor);
-          return this;
-        };
+  const http = new XMLHttpRequest();
+  var yumRepo = 'https://update.cybersponse.com/';
+  var listItems = [];
+  var filter = 'all';
+  var listItemsBkp;
+
+  function init() {
+    var allItemsJson = $.getJSON({'url': "assets/info.json", 'async': false});
+    allItemsJson = JSON.parse(allItemsJson.responseText);
+    totalItems = allItemsJson.length;
+    listItems = allItemsJson;
+    listItemsBkp = listItems;
+    buildListData(listItems);
+  }();
+
+  function applyFilter(type, event) {
+    var filteredListItems = [];
+    $("ul.sidebar-nav a").removeClass("active");
+    $(event.target).addClass("active");
+    listItems.forEach(function(item) {
+      if(item.type === type) {
+        filteredListItems.push(item); 
       }
-    ]);
-  })();
+    });
+    
+    buildListData(filteredListItems);
+  };
+
+  function openDetails(detail) {
+    var detailPath;
+    var downloadPath;
+    var detailInfo;
+    if(detail.type === 'connector'){
+      detailPath = yumRepo + 'connectors/info/' + detail.name + '_' + detail.version + '/info.json';
+      downloadPath = yumRepo + 'connectors/x86_64/' + detail.rpm_full_name;
+    } else if(detail.type === 'widget') {
+      detailPath = yumRepo + 'fsr-widgets/' + detail.name + '-' + detail.version + '/info.json';
+      downloadPath = yumRepo + 'fsr-widgets/' + detail.name + '-' + detail.version + '/' + detail.name + '-' + detail.version + '.tgz';
+    }
+
+    $http.get(detailPath).then(function(response) {
+      detailInfo = response.data;
+      detailInfo.display = detail.display;
+      detailInfo.type = detail.type;
+      detailInfo.downloadPath = downloadPath;
+    });
+  };
+
+  function submitSearch(searchText) {
+    if(searchText.length >= 3) {
+      var searchedListItems = [];
+      listItems.forEach(function(item) {
+        if(item.display.toLowerCase().indexOf(searchText.toLowerCase()) > -1) {
+          searchedListItems.push(item); 
+        }
+      });
+      listItems = searchedListItems;
+    } else {
+      listItems = listItemsBkp;
+    }
+    buildListData(listItems);
+  };
+
+  function downloadFile(detail) {
+    var downloadFileElement = document.createElement('a');
+    downloadFileElement.href = detail.downloadPath;
+    downloadFileElement.target = '_blank';
+    downloadFileElement.download = detail.name + '-' + detail.version;
+    document.body.appendChild(downloadFileElement);
+    downloadFileElement.click();
+    document.body.removeChild(downloadFileElement);
+  };
+
+  function buildListData(listData) {
+    var allListItems;
+    listData.forEach(function(listItem) {
+      allListItems = '<div class="pull-left text-center item-container" onClick="openDetails('+listItem+')"><div class="item-icon">';
+      if(item.type !== 'connector'){
+        allListItems += '<img src="assets/images/icon_large.png" />';
+      } else {
+        allListItems += '<img src="' +listItem.iconLarge + '" />';
+      }
+      allListItems += '</div><h5>' + listItem.display + '</h5><div class="card-footer"><a class="card-link" href="#" title="GitHub Page" target="_blank"> <span class="fa fa-github"></span></a>';
+      allListItems += '<a class="card-link" href="#" title="Forks" target="_blank"> <span class="fa fa-code-fork"></span>' + listItem.forks_count + '</a>';
+      allListItems += '<a class="card-link" href="#" title="Stargazers" target="_blank"> <span class="fa fa-star"></span>' + listItem.stargazers_count + '</a></div></div>';
+      $("#marketplace-list").appendChild(allListItems);
+    });
+  }
