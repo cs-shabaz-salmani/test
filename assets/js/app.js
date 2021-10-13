3fca2c26ebaca62f518d0434c555de7811035620
Shabaz Salmani <ssalmani@fortinet.com>
2021-10-13 18:06:55 +0530

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -24,7 +24,7 @@
   init();
 
   function applyFilter(event, type) {
-    if (window.location.href.indexOf('detail.html') > -1) {
+    if (window.location.href.indexOf('list.html') == 0) {
       window.location.href = "/list.html";
     }
     var filteredListItems = [];
