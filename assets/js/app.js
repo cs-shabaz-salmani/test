589f4dfdb9a628b67dde48f1a1c5e536368634ad
Shabaz Salmani <ssalmani@fortinet.com>
2022-01-28 12:44:40 +0530

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -186,7 +186,8 @@
       aTaglistItem.href = basePath + "detail.html?entity=" + listItem.name + "&version=" + listItem.version + "&type=" + listItem.type;
       aTaglistItem.className = "mp-tile-container";
       aTaglistItem.setAttribute("rel", "canonical");
-      aTaglistItem.setAttribute("onClick", "setDetailsInLocal(listItem.infoPath)");
+      var infoPath = listItem.infoPath;
+      aTaglistItem.setAttribute("onClick", "setDetailsInLocal(infoPath)");
       
       var itemIconSpan = document.createElement('span');
       itemIconSpan.className = "mp-content-type-icon pull-left margin-top-2";
