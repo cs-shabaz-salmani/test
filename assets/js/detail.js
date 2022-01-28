f0cf7abf0ff043554d387aa2bd53ef24d685ed99
Shabaz Salmani <ssalmani@fortinet.com>
2022-01-28 18:10:12 +0530

    Update detail.js

--- assets/js/detail.js
+++ assets/js/detail.js
@@ -41,7 +41,7 @@
       document.getElementById("detail-release-notes").append(releaseNotes);
       var docContent = document.createElement('zero-md');
       docContent.setAttribute("src", "https://raw.githubusercontent.com/fortinet-fortisoar/solution-pack-symantec-solutions/develop/README.md");
-      document.getElementById("detail-docs-content").append(docContent);
+      document.getElementById("detail-docs-content").append(docContent); 
 //       httpGetAsync(mdFilepath, function(fileResponse) {
 //         document.getElementById("detail-release-notes").innerHTML = fileResponse;
 //       });
