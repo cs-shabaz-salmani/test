2bba707987d908851a2aa3c3886e51dcdd1fe9b3
Shabaz Salmani <ssalmani@fortinet.com>
2022-02-16 11:55:22 +0530

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -446,7 +446,7 @@
       var cardDescription = document.createElement('p');
       cardDescription.className = "card-description mp-tile-description muted-80"; //remove card-description class
       listItem.description = listItem.description ? listItem.description : 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...';
-      var itemDescription = document.createTextNode(listItem.description.substring(0, 90) + '...');
+      var itemDescription = document.createTextNode(listItem.description.substring(0, 145) + '...');
       cardDescription.appendChild(itemDescription);
       aTaglistItem.appendChild(cardDescription);
       marketPlace.append(aTaglistItem);
