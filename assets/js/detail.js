c210bac58df5e0a78e6de05ce75aac0f1d697ca0
Shabaz Salmani <ssalmani@fortinet.com>
2022-04-06 15:11:45 +0530

    Update detail.js

--- assets/js/detail.js
+++ assets/js/detail.js
@@ -38,19 +38,20 @@
       if(detailInfo.availableVersions.length > 0){
         detailAvailableVersions.classList.remove("d-none");
         _.each(detailInfo.availableVersions, function(version) {
-          var versionTag = document.createElement('li');
+          var versionTag = document.createElement('a');
           versionTag.className = version !== detailInfo.version ? "dropdown-item" : "dropdown-item";
-          if(version !== detailInfo.version) {
-            versionTag.onclick = function(){
-              getBuildNumber(detailInfo.name, version, detailInfo.type);
-            };
+          var buildNumber = version === detailInfo.version ? detailInfo.buildNumber : "latest";
+//           if(version !== detailInfo.version) {
+//             versionTag.onclick = function(){
+//               getBuildNumber(detailInfo.name, version, detailInfo.type);
+//             };
 //             versionTag.addEventListener("click", function () {
 //               getBuildNumber(detailInfo.name, version, detailInfo.type);
 //             });
-          } else {
-//             versionTag.href = basePath + "detail.html?entity=" + detailInfo.name + "&version=" + version + "&type=" + detailInfo.type + "&buildNumber=" + detailInfo.buildNumber;
-//             versionTag.setAttribute("target", "_self");
-          }
+//           } else {
+            versionTag.href = basePath + "detail.html?entity=" + detailInfo.name + "&version=" + version + "&type=" + detailInfo.type + "&buildNumber=" + buildNumber;
+            versionTag.setAttribute("target", "_self");
+//           }
           var versionText = document.createTextNode("Version - " + version);
           versionTag.append(versionText);
           detailAvailableVersions.append(versionTag);
