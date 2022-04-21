9370273875126e31dae716bed94a16b6d4902256
Shabaz Salmani <ssalmani@fortinet.com>
2022-04-21 15:50:26 +0530

    Update detail.js

--- assets/js/detail.js
+++ assets/js/detail.js
@@ -45,6 +45,7 @@
           var versionTag = document.createElement('a');
           versionTag.className = "btn btn-link dropdown-item rounded-0";
           versionTag.setAttribute("href", basePath + "detail.html?entity=" + detailInfo.name + "&version=" + version + "&type=" + detailInfo.type);
+          versionTag.setAttribute("target", "_self");
 //           var buildNumber = version === detailInfo.version ? detailInfo.buildNumber : "latest";
 //           if(version !== detailInfo.version) {
 //             versionTag.onclick = function(){
